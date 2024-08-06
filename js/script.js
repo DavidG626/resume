be191d419099114e83256477c1e0a68af0cc60a1
DavidG626 <davidguticodes@gmail.com>
2024-08-06 12:28:52 -0700

    updated js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,26 @@ function toggleMenu() {
     menu.classList.toggle('open');
     icon.classList.toggle('open');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const logoDiv = document.querySelector('.logo');
+    const logoLink = logoDiv.querySelector('a');
+    const originalText = logoLink.textContent;
+    const originalHref = logoLink.getAttribute('href');
+
+    logoDiv.addEventListener('mouseover', function() {
+        logoLink.textContent = '(source code)';
+        logoLink.setAttribute('href', 'https://github.com/DavidG626/resume');
+        logoLink.setAttribute('target', '_blank');
+    });
+
+    logoDiv.addEventListener('mouseout', function() {
+        logoLink.textContent = originalText;
+        logoLink.setAttribute('href', originalHref);
+        logoLink.removeAttribute('target');
+    });
+});
+   
+
+
+
