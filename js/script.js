65e5e973d833aa577d12cfeba54bf72b71487fca
DavidG626 <davidguticodes@gmail.com>
2024-08-07 16:00:16 -0700

    updated css

--- js/script.js
+++ js/script.js
@@ -15,29 +15,28 @@ function toggleMenu() {
 function setupLogoInteraction() {
     console.log("Setting up logo interaction");
     
-    // Use event delegation on the document body
     document.body.addEventListener('mouseover', function(event) {
-        const logoLink = event.target.closest('.logo');
-        if (logoLink) {
-            console.log("Mouseover event triggered on logo");
-            logoLink.textContent = '(source code)';
-            logoLink.setAttribute('href', 'https://github.com/DavidG626/resume');
-            logoLink.setAttribute('target', '_blank');
-        }
+      const logoLink = event.target.closest('.logo');
+      if (logoLink) {
+        console.log("Mouseover event triggered on logo");
+        logoLink.textContent = '(source code)';
+        logoLink.setAttribute('href', 'https://github.com/DavidG626/resume');
+        logoLink.setAttribute('target', '_blank');
+      }
     });
 
     document.body.addEventListener('mouseout', function(event) {
-        const logoLink = event.target.closest('.logo');
-        if (logoLink) {
-            console.log("Mouseout event triggered on logo");
-            logoLink.textContent = 'David Gutierrez'; 
-            logoLink.setAttribute('href', '#');
-            logoLink.removeAttribute('target');
-        }
+      const logoLink = event.target.closest('.logo');
+      if (logoLink) {
+        console.log("Mouseout event triggered on logo");
+        logoLink.textContent = 'David Gutierrez';
+        logoLink.setAttribute('href', '#');
+        logoLink.removeAttribute('target');
+      }
     });
-}
+  }
 
-document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('DOMContentLoaded', function() {
     console.log("DOMContentLoaded event triggered");
     setupLogoInteraction();
-});
+  });
